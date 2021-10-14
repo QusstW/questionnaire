1d594ec523ac932bc7d4d7dbed45dd6b311d17e0
Daniil_Evseev <qusstid@gmail.com>
2021-10-14 22:42:25 +0300

    last fix | add theme provider

--- src/app/App.js
+++ src/app/App.js
@@ -4,26 +4,40 @@ import { MainLayout, NavMenu } from "../components";
 import { StartPage, Questionnaire, Tester, Welcome } from "../pages";
 import { DESIGNER, FRONTEND } from "../constants";
 import { BrowserRouter, Route } from "react-router-dom";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
 
 const App = () => {
+  const theme = createTheme({
+    palette: {
+      primary: {
+        main: "#222222",
+      },
+      secondary: {
+        main: "#18ffff",
+      },
+    },
+  });
+
   return (
     <BrowserRouter>
-      <CalcProvider>
-        <NavMenu />
-        <MainLayout>
-          <Welcome />
-          <Route path="/start" render={() => <StartPage />} />
-          <Route
-            path="/designer"
-            render={() => <Questionnaire data={DESIGNER} />}
-          />
-          <Route
-            path="/frontend"
-            render={() => <Questionnaire data={FRONTEND} />}
-          />
-          <Route path="/tester" render={() => <Tester />} />
-        </MainLayout>
-      </CalcProvider>
+      <ThemeProvider theme={theme}>
+        <CalcProvider>
+          <NavMenu />
+          <MainLayout>
+            <Welcome />
+            <Route path="/start" render={() => <StartPage />} />
+            <Route
+              path="/designer"
+              render={() => <Questionnaire data={DESIGNER} />}
+            />
+            <Route
+              path="/frontend"
+              render={() => <Questionnaire data={FRONTEND} />}
+            />
+            <Route path="/tester" render={() => <Tester />} />
+          </MainLayout>
+        </CalcProvider>
+      </ThemeProvider>
     </BrowserRouter>
   );
 };
